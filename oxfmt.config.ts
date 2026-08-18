a93187a014a238d95e354df8446333472479d637
Mikhail Priver <m.priver@gmail.com>
2026-08-18 12:23:14 +0300

    chore: add @priver/tsconfig, @priver/oxlint-config, and @priver/oxfmt-config packages

--- oxfmt.config.ts
+++ oxfmt.config.ts
@@ -1,39 +1,11 @@
+import config from '@priver/oxfmt-config';
 import { defineConfig } from 'oxfmt';
 
 export default defineConfig({
-  ignorePatterns: [
-    '**/*.html',
-    'pnpm-lock.yaml',
-    '.agents/skills/',
-    'apps/reader/src/routeTree.gen.ts',
-  ],
-  singleQuote: true,
-  quoteProps: 'consistent',
-  proseWrap: 'always',
-  jsdoc: true,
-  sortImports: {
-    ignoreCase: false,
-    internalPattern: ['@priver/'],
-    groups: [
-      'builtin',
-      'external',
-      'internal',
-      'subpath',
-      ['parent', 'sibling', 'index'],
-      'style',
-      'unknown',
-    ],
-  },
+  ...config,
+  ignorePatterns: ['**/*.html', 'pnpm-lock.yaml', '**/routeTree.gen.ts'],
   sortTailwindcss: {
-    stylesheet: './packages/ui/src/style.css',
+    stylesheet: './apps/reader/src/style.css',
     functions: ['cva', 'cx'],
   },
-  overrides: [
-    {
-      files: ['**/CHANGELOG.md', '.changeset/*.md'],
-      options: {
-        proseWrap: 'preserve',
-      },
-    },
-  ],
 });
